43e3e667fe11af8585a8e5f16de47d6491013922
fahmid munshi <fahmidmunshi2004@gmail.com>
2026-03-29 13:00:27 +0600

    pricing-done

--- js/tab.js
+++ js/tab.js
@@ -18,3 +18,30 @@ document.querySelectorAll('.tab-wrapper').forEach(wrapper => {
     });
 
 });
+
+
+
+//======================================== switch tab ===========================================//
+document.addEventListener("DOMContentLoaded", function () {
+
+    const surgeToggle = document.querySelector(".surge-toggle input[type='checkbox']");
+    const serviceToggle = document.querySelector(".service-toggle input[type='checkbox']");
+
+    const info1 = document.querySelectorAll(".info-1");
+    const info2 = document.querySelectorAll(".info-2");
+
+    function updateInfo() {
+        if (surgeToggle.checked && serviceToggle.checked) {
+            info1.forEach(el => el.classList.add("hidden"));
+            info2.forEach(el => el.classList.remove("hidden"));
+        } else {
+            info1.forEach(el => el.classList.remove("hidden"));
+            info2.forEach(el => el.classList.add("hidden"));
+        }
+    }
+
+    // event listeners //
+    surgeToggle.addEventListener("change", updateInfo);
+    serviceToggle.addEventListener("change", updateInfo);
+
+});
